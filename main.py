d8c1b8920070f21022ea44e8fcf3151f348b1c2a
Khushi Pitroda <72183841+happy522@users.noreply.github.com>
2023-08-30 00:13:44 +0530

    Update main.py
    Version 2 with TDD testing each time (UNSTABLE)

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
+import unittest
+
 class GalacticSpacecraft:
     def __init__(self, x, y, z, direction):
+        # Initialize the spacecraft's position and direction
         self.x = x
         self.y = y
         self.z = z
         self.direction = direction
 
     def move_forward(self):
+        # Move the spacecraft forward based on its current direction
         if self.direction == 'N':
             self.y += 1
         elif self.direction == 'S':
@@ -20,6 +24,7 @@ class GalacticSpacecraft:
             self.z -= 1
 
     def move_backward(self):
+        # Move the spacecraft backward based on its current direction
         if self.direction == 'N':
             self.y -= 1
         elif self.direction == 'S':
@@ -34,6 +39,7 @@ class GalacticSpacecraft:
             self.z += 1
 
     def turn_left(self):
+        # Turn the spacecraft 90 degrees to the left
         if self.direction == 'N':
             self.direction = 'W'
         elif self.direction == 'S':
@@ -44,6 +50,7 @@ class GalacticSpacecraft:
             self.direction = 'S'
 
     def turn_right(self):
+        # Turn the spacecraft 90 degrees to the right
         if self.direction == 'N':
             self.direction = 'E'
         elif self.direction == 'S':
@@ -54,6 +61,7 @@ class GalacticSpacecraft:
             self.direction = 'N'
 
     def turn_up(self):
+        # Turn the spacecraft's angle upwards
         if self.direction == 'N':
             self.direction = 'Up'
         elif self.direction == 'S':
@@ -64,6 +72,7 @@ class GalacticSpacecraft:
             self.direction = 'N'
 
     def turn_down(self):
+        # Turn the spacecraft's angle downwards
         if self.direction == 'N':
             self.direction = 'Down'
         elif self.direction == 'S':
@@ -74,6 +83,7 @@ class GalacticSpacecraft:
             self.direction = 'S'
 
     def execute_commands(self, commands):
+        # Execute the sequence of commands to control the spacecraft
         for command in commands:
             if command == 'f':
                 self.move_forward()
@@ -88,13 +98,26 @@ class GalacticSpacecraft:
             elif command == 'd':
                 self.turn_down()
 
-# Example usage
-starting_position = (0, 0, 0)
-initial_direction = 'N'
-commands = ['f', 'r', 'u', 'b', 'l']
+class TestGalacticSpacecraft(unittest.TestCase):
+   def test_execute_commands(self):
+        # Test the execute_commands method with corrected example commands
+        starting_position = (0, 0, 0)
+        initial_direction = 'N'
+        commands = ['f', 'r', 'u', 'b', 'l']
 
-spacecraft = GalacticSpacecraft(*starting_position, initial_direction)
-spacecraft.execute_commands(commands)
+        spacecraft = GalacticSpacecraft(*starting_position, initial_direction)
+        spacecraft.execute_commands(commands)
 
-print("Final Position:", (spacecraft.x, spacecraft.y, spacecraft.z))
-print("Final Direction:", spacecraft.direction)
+        final_position = (spacecraft.x, spacecraft.y, spacecraft.z)
+        final_direction = spacecraft.direction
+
+        # Verify the final position and direction after executing commands
+        self.assertEqual(final_position, (0, 1, 0))
+        self.assertEqual(final_direction, 'N')
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+

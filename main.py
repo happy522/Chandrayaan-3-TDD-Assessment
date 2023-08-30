514607dcf9d802948ad4554c8b26d5be9b5c494d
Khushi Pitroda <72183841+happy522@users.noreply.github.com>
2023-08-30 12:14:26 +0530

    Update main.py
    Version - 6 Handling exceptions

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import unittest
 
+
 class GalacticSpacecraft:
     def __init__(self, x, y, z, direction):
         # Initialize the spacecraft's position (x, y, z) and direction (N, S, E, W, Up, Down)
@@ -10,98 +11,99 @@ class GalacticSpacecraft:
 
     def move_forward(self):
         # Move the spacecraft one step forward based on its current direction
-        if self.direction == 'N':
+        if self.direction == "N":
             self.y += 1
-        elif self.direction == 'S':
+        elif self.direction == "S":
             self.y -= 1
-        elif self.direction == 'E':
+        elif self.direction == "E":
             self.x += 1
-        elif self.direction == 'W':
+        elif self.direction == "W":
             self.x -= 1
-        elif self.direction == 'Up':
+        elif self.direction == "Up":
             self.z += 1
-        elif self.direction == 'Down':
+        elif self.direction == "Down":
             self.z -= 1
 
     def move_backward(self):
         # Move the spacecraft one step backward based on its current direction
-        if self.direction == 'N':
+        if self.direction == "N":
             self.y -= 1
-        elif self.direction == 'S':
+        elif self.direction == "S":
             self.y += 1
-        elif self.direction == 'E':
+        elif self.direction == "E":
             self.x -= 1
-        elif self.direction == 'W':
+        elif self.direction == "W":
             self.x += 1
-        elif self.direction == 'Up':
+        elif self.direction == "Up":
             self.z -= 1
-        elif self.direction == 'Down':
+        elif self.direction == "Down":
             self.z += 1
 
     def turn_left(self):
         # Turn the spacecraft 90 degrees to the left
-        if self.direction == 'N':
-            self.direction = 'W'
-        elif self.direction == 'S':
-            self.direction = 'E'
-        elif self.direction == 'E':
-            self.direction = 'N'
-        elif self.direction == 'W':
-            self.direction = 'S'
+        if self.direction == "N":
+            self.direction = "W"
+        elif self.direction == "S":
+            self.direction = "E"
+        elif self.direction == "E":
+            self.direction = "N"
+        elif self.direction == "W":
+            self.direction = "S"
 
     def turn_right(self):
         # Turn the spacecraft 90 degrees to the right
-        if self.direction == 'N':
-            self.direction = 'E'
-        elif self.direction == 'S':
-            self.direction = 'W'
-        elif self.direction == 'E':
-            self.direction = 'S'
-        elif self.direction == 'W':
-            self.direction = 'N'
+        if self.direction == "N":
+            self.direction = "E"
+        elif self.direction == "S":
+            self.direction = "W"
+        elif self.direction == "E":
+            self.direction = "S"
+        elif self.direction == "W":
+            self.direction = "N"
 
     def turn_up(self):
         # Turn the spacecraft upwards
-        if self.direction == 'N':
-            self.direction = 'Up'
-        elif self.direction == 'S':
-            self.direction = 'Down'
-        elif self.direction == 'Up':
-            self.direction = 'S'
-        elif self.direction == 'Down':
-            self.direction = 'N'
+        if self.direction == "N":
+            self.direction = "Up"
+        elif self.direction == "S":
+            self.direction = "Down"
+        elif self.direction == "Up":
+            self.direction = "S"
+        elif self.direction == "Down":
+            self.direction = "N"
 
     def turn_down(self):
         # Turn the spacecraft downwards
-        if self.direction == 'N':
-            self.direction = 'Down'
-        elif self.direction == 'S':
-            self.direction = 'Up'
-        elif self.direction == 'Up':
-            self.direction = 'N'
-        elif self.direction == 'Down':
-            self.direction = 'S'
+        if self.direction == "N":
+            self.direction = "Down"
+        elif self.direction == "S":
+            self.direction = "Up"
+        elif self.direction == "Up":
+            self.direction = "N"
+        elif self.direction == "Down":
+            self.direction = "S"
 
     def execute_commands(self, commands):
         # Execute a sequence of commands to control the spacecraft
         for command in commands:
-            if command == 'f':
+            if command == "f":
                 self.move_forward()  # Move the spacecraft forward
-            elif command == 'b':
+            elif command == "b":
                 self.move_backward()  # Move the spacecraft backward
-            elif command == 'l':
+            elif command == "l":
                 self.turn_left()  # Turn the spacecraft left
-            elif command == 'r':
+            elif command == "r":
                 self.turn_right()  # Turn the spacecraft right
-            elif command == 'u':
+            elif command == "u":
                 self.turn_up()  # Turn the spacecraft up
-            elif command == 'd':
+            elif command == "d":
                 self.turn_down()  # Turn the spacecraft down
+
 
 # Implementation without TDD
 starting_position = (0, 0, 0)
-initial_direction = 'N'
-commands = ['f', 'r', 'u', 'b', 'l']
+initial_direction = "N"
+commands = ["f", "r", "u", "b", "l"]
 
 spacecraft_without_tdd = GalacticSpacecraft(*starting_position, initial_direction)
 spacecraft_without_tdd.execute_commands(commands)
@@ -109,10 +111,11 @@ spacecraft_without_tdd.execute_commands(commands)
 print("\n--------------Default Input----------------------\n")
 
 print("Without TDD:")
-print("Final Position:", (spacecraft_without_tdd.x, spacecraft_without_tdd.y, spacecraft_without_tdd.z))
+print(
+    "Final Position:",
+    (spacecraft_without_tdd.x, spacecraft_without_tdd.y, spacecraft_without_tdd.z),
+)
 print("Final Direction:", spacecraft_without_tdd.direction)
-
-
 
 
 # Implementation with TDD
@@ -120,8 +123,8 @@ class TestGalacticSpacecraft(unittest.TestCase):
     def test_execute_commands(self):
         # Test the execute_commands method with example commands
         starting_position = (0, 0, 0)
-        initial_direction = 'N'
-        commands = ['f', 'r', 'u', 'b', 'l']
+        initial_direction = "N"
+        commands = ["f", "r", "u", "b", "l"]
 
         # Create a spacecraft instance
         spacecraft_with_tdd = GalacticSpacecraft(*starting_position, initial_direction)
@@ -129,34 +132,60 @@ class TestGalacticSpacecraft(unittest.TestCase):
         spacecraft_with_tdd.execute_commands(commands)
 
         # Verify the final position and direction
-        final_position = (spacecraft_with_tdd.x, spacecraft_with_tdd.y, spacecraft_with_tdd.z)
+        final_position = (
+            spacecraft_with_tdd.x,
+            spacecraft_with_tdd.y,
+            spacecraft_with_tdd.z,
+        )
         final_direction = spacecraft_with_tdd.direction
 
         # Check if the final position and direction match the expected values
-        self.assertEqual(final_position, (-1,1,0))
-        self.assertEqual(final_direction, 'N')
+        self.assertEqual(final_position, (-1, 1, 0))
+        self.assertEqual(final_direction, "N")
 
-print('\n---------------Input from user------------------\n')
+
+print("\n---------------Input from user------------------\n")
+
+def get_coordinate_input(prompt):
+    while True:
+        coordinate = input(prompt)
+        if coordinate.isdigit():
+            return int(coordinate)
+        else:
+            print("Invalid input. Please enter a valid digit.")
+
 # Input from the user
-x = int(input("Enter the starting x-coordinate: "))
-y = int(input("Enter the starting y-coordinate: "))
-z = int(input("Enter the starting z-coordinate: "))
+x = get_coordinate_input("Enter the starting x-coordinate: ")
+y = get_coordinate_input("Enter the starting y-coordinate: ")
+z = get_coordinate_input("Enter the starting z-coordinate: ")
 initial_direction = input("Enter the initial direction (N, S, E, W, Up, Down): ")
 commands = input("Enter the commands as a string (e.g., 'frubl'): ")
 
 # Create a spacecraft instance
 spacecraft = GalacticSpacecraft(x, y, z, initial_direction)
-# Execute the sequence of commands
-spacecraft.execute_commands(commands)
 
-# Calculate the final position and direction
-final_position = (spacecraft.x, spacecraft.y, spacecraft.z)
-final_direction = spacecraft.direction
+# Valid command options
+valid_commands = "frubld"
 
-# Display the final results
-print("Final Position:", final_position)
-print("Final Direction:", final_direction)
+try:
+    # Execute the sequence of commands
+    for command in commands:
+        if command not in valid_commands:
+            raise ValueError("Invalid command: {}".format(command))
+        spacecraft.execute_commands(command)
+except ValueError as e:
+    print("Error:", e)
+else:
+    # Calculate the final position and direction
+    final_position = (spacecraft.x, spacecraft.y, spacecraft.z)
+    final_direction = spacecraft.direction
 
-if __name__ == '__main__':
+    # Display the final results
+    print("Final Position:", final_position)
+    print("Final Direction:", final_direction)
+
+
+# for TDD - unit test
+if __name__ == "__main__":
     print("\nWith TDD:")
     unittest.main()

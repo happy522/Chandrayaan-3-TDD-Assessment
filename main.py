9194a6593764db983dc1a6db6af67d8616f61523
Khushi Pitroda <72183841+happy522@users.noreply.github.com>
2023-08-30 11:45:26 +0530

    Update main.py
    Version - 5 (Input from user)

--- main.py
+++ main.py
@@ -106,9 +106,14 @@ commands = ['f', 'r', 'u', 'b', 'l']
 spacecraft_without_tdd = GalacticSpacecraft(*starting_position, initial_direction)
 spacecraft_without_tdd.execute_commands(commands)
 
+print("\n--------------Default Input----------------------\n")
+
 print("Without TDD:")
 print("Final Position:", (spacecraft_without_tdd.x, spacecraft_without_tdd.y, spacecraft_without_tdd.z))
 print("Final Direction:", spacecraft_without_tdd.direction)
+
+
+
 
 # Implementation with TDD
 class TestGalacticSpacecraft(unittest.TestCase):
@@ -130,6 +135,27 @@ class TestGalacticSpacecraft(unittest.TestCase):
         # Check if the final position and direction match the expected values
         self.assertEqual(final_position, (-1,1,0))
         self.assertEqual(final_direction, 'N')
+
+print('\n---------------Input from user------------------\n')
+# Input from the user
+x = int(input("Enter the starting x-coordinate: "))
+y = int(input("Enter the starting y-coordinate: "))
+z = int(input("Enter the starting z-coordinate: "))
+initial_direction = input("Enter the initial direction (N, S, E, W, Up, Down): ")
+commands = input("Enter the commands as a string (e.g., 'frubl'): ")
+
+# Create a spacecraft instance
+spacecraft = GalacticSpacecraft(x, y, z, initial_direction)
+# Execute the sequence of commands
+spacecraft.execute_commands(commands)
+
+# Calculate the final position and direction
+final_position = (spacecraft.x, spacecraft.y, spacecraft.z)
+final_direction = spacecraft.direction
+
+# Display the final results
+print("Final Position:", final_position)
+print("Final Direction:", final_direction)
 
 if __name__ == '__main__':
     print("\nWith TDD:")
